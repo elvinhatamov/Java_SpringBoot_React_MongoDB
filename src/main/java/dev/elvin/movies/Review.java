36db392f4123ad5950b37d48c305b4b2ccc94875
Elvin Hatamov <58266209+elvinhatamov@users.noreply.github.com>
2023-01-27 17:01:15 -0500

    write review

--- src/main/java/dev/elvin/movies/Review.java
+++ src/main/java/dev/elvin/movies/Review.java
@@ -2,6 +2,7 @@ package dev.elvin.movies;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -9,9 +10,15 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Review {
+
     @Id
 
     private ObjectId id;
     private String body;
+
+    public Review(String body) {
+        this.body = body;
+    }
 }
